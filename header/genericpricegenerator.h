5ddd8cf4c3ba424967632da3601fe4129b291069
AssSeed <108232415+AssSeed@users.noreply.github.com>
2020-10-25 08:03:28 +0000

    Initial Cookiecutter Commit.

--- header/genericpricegenerator.h
+++ header/genericpricegenerator.h
@@ -1,2 +1,13 @@
 #ifndef GENERICPRICEGENERATOR_H
-#define GENERICPRICE
+#define GENERICPRICEGENERATOR_H
+
+#include <QObject>
+
+class GenericPriceGenerator : public QObject
+{
+    Q_OBJECT
+public:
+    explicit GenericPriceGenerator(QObject *parent = 0);
+
+    virtual void setRange(int ymax = 100) = 0;
+    virtual int getRange(void
