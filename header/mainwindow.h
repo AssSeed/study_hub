aa926d33f1f6a7e06284b2469e7794fa51de006a
AssSeed <108232415+AssSeed@users.noreply.github.com>
2020-10-27 14:31:56 +0000

    sync: Only print details for active fences

--- header/mainwindow.h
+++ header/mainwindow.h
@@ -1,1 +1,13 @@
-#if
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QVector>
+#include <QTimer>
+
+#include "stockpricehistoryplot.h"
+#include "moneyavailable.h"
+
+const int default_initial_money = 10000;
+const int max_interval = 400;
+extern unsigned i
