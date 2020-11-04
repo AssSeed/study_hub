dfd064212d7f14f2dfc9e445a128e830f51a3705
AssSeed <108232415+AssSeed@users.noreply.github.com>
2020-11-04 10:30:41 +0000

    spi: Fix documentation of spi_alloc_master()

--- header/singlestock.h
+++ header/singlestock.h
@@ -4,4 +4,13 @@
 #include <QWidget>
 #include <QTimer>
 
-#
+#include <stockpricehistoryplot.h>
+
+namespace Ui {
+class SingleStock;
+}
+
+/*
+ * This is the UI class consisting the Buy/Sell buttons, the price LCDs,
+ * the price diagram and the buy step spin box.
+ * The company behind the graph is place
