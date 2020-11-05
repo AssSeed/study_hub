323f61a9ab6e0357a249e2430926cdd565f0d7b8
AssSeed <108232415+AssSeed@users.noreply.github.com>
2020-11-05 05:45:32 +0000

    fix man page

--- header/singlestock.h
+++ header/singlestock.h
@@ -13,4 +13,19 @@ class SingleStock;
 /*
  * This is the UI class consisting the Buy/Sell buttons, the price LCDs,
  * the price diagram and the buy step spin box.
- * The company behind the graph is place
+ * The company behind the graph is placed in the plot widget (ui->plot)
+ */
+class SingleStock : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit SingleStock(QWidget *parent = 0);
+    ~SingleStock();
+
+public slots:
+    void buyStock(void);
+    void sellStock(void);
+
+private slots:
+    void changeBuyStep(int)
