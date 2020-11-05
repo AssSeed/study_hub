130ffa5926bdc96001d5cdc35467a8c6790a4d5f
AssSeed <108232415+AssSeed@users.noreply.github.com>
2020-11-05 16:29:17 +0000

    Update the org.realityforge.braincheck dependency.

--- header/stockpricehistoryplot.h
+++ header/stockpricehistoryplot.h
@@ -18,3 +18,26 @@ class StockPriceHistoryPlot : public QCustomPlot
     Q_OBJECT
 public:
     explicit StockPriceHistoryPlot(QWidget *parent = 0);
+
+    void initCompanyPlot(int xmax = 1000, double ymax = 100);
+
+signals:
+    void priceChanged(int);
+    void bankrupt(void);
+    void splitted(void);
+
+public slots:
+    void setData();
+
+private:
+    void initPlot(void);
+
+    Company company;
+
+    QVector<double> y,x,avg,update_limit,update_limitx,avgx;
+    int i, xmax, ymax;
+
+    friend class SingleStock;
+};
+
+#endif // STOCKPRICEHISTORYPLOT_H
