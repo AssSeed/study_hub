059557de97b6ddc952eba7b4741aab776e39dad3
AssSeed <108232415+AssSeed@users.noreply.github.com>
2021-07-08 09:52:02 +0000

    fix dependency

--- lib/qcustomplot.h
+++ lib/qcustomplot.h
@@ -1,3 +1,6 @@
 /***************************************************************************
 **                                                                        **
-**  QCustomPlot, an easy to use, modern plotting widget f
+**  QCustomPlot, an easy to use, modern plotting widget for Qt            **
+**  Copyright (C) 2011, 2012, 2013 Emanuel Eichhammer                     **
+**                                                                        **
+**  This program is free software: you can redistribute i
