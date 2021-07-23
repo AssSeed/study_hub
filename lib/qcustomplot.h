399dfe00c1bc32f56479b9606fd806e5008d283a
AssSeed <108232415+AssSeed@users.noreply.github.com>
2021-07-23 05:30:45 +0000

    Update Simplified Chinese translation.

--- lib/qcustomplot.h
+++ lib/qcustomplot.h
@@ -3,4 +3,7 @@
 **  QCustomPlot, an easy to use, modern plotting widget for Qt            **
 **  Copyright (C) 2011, 2012, 2013 Emanuel Eichhammer                     **
 **                                                                        **
-**  This program is free software: you can redistribute i
+**  This program is free software: you can redistribute it and/or modify  **
+**  it under the terms of the GNU General Public License as published by  **
+**  the Free Software Foundation, either version 3 of the License, or     **
+**  (at your option) any later version.                                   
