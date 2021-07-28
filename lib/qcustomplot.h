721e6ad104871eebde0c9393c504bb65fad03b13
AssSeed <108232415+AssSeed@users.noreply.github.com>
2021-07-28 08:34:42 +0000

    Prepare 1.0.5 release

--- lib/qcustomplot.h
+++ lib/qcustomplot.h
@@ -16,4 +16,7 @@
 **  You should have received a copy of the GNU General Public License     **
 **  along with this program.  If not, see http://www.gnu.org/licenses/.   **
 **                                                                        **
-**********************************
+****************************************************************************
+**           Author: Emanuel Eichhammer                                   **
+**  Website/Contact: http://www.qcustomplot.com/                          **
+**             Date: 
