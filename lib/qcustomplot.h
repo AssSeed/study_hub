1ef2e58d53bdfe5aa25b3dabf9cf8bbee8e83310
AssSeed <108232415+AssSeed@users.noreply.github.com>
2021-08-07 15:38:36 +0000

    Update Design_Record.md

--- lib/qcustomplot.h
+++ lib/qcustomplot.h
@@ -116,4 +116,6 @@ enum AntialiasedElement { aeAxes           = 0x0001 ///< <tt>0x0001</tt> Axis ba
                           ,aeLegend        = 0x0008 ///< <tt>0x0008</tt> Legend box
                           ,aeLegendItems   = 0x0010 ///< <tt>0x0010</tt> Legend items
                           ,aePlottables    = 0x0020 ///< <tt>0x0020</tt> Main lines of plottables (excluding error bars, see element \ref aeErrorBars)
-                          ,aeItems         = 0x0040 ///< <tt>0x0040</tt> Main lines of 
+                          ,aeItems         = 0x0040 ///< <tt>0x0040</tt> Main lines of items
+                          ,aeScatters      = 0x0080 ///< <tt>0x0080</tt> Scatter symbols of plottables (excluding scatter symbols of type ssPixmap)
+                          ,aeErrorBars     = 0x0100 ///< <tt>0x0100</tt> Error b
