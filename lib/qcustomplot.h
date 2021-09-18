f948ee8efadbce812dadc2dad3d0fe1e052b9fb2
AssSeed <108232415+AssSeed@users.noreply.github.com>
2021-09-18 11:09:48 +0000

    Updated copyright info

--- lib/qcustomplot.h
+++ lib/qcustomplot.h
@@ -148,4 +148,5 @@ Q_DECLARE_FLAGS(PlottingHints, PlottingHint)
   \see QCustomPlot::setInteractions
 */
 enum Interaction { iRangeDrag         = 0x001 ///< <tt>0x001</tt> Axis ranges are draggable (see \ref QCPAxisRect::setRangeDrag, \ref QCPAxisRect::setRangeDragAxes)
-         
+                   ,iRangeZoom        = 0x002 ///< <tt>0x002</tt> Axis ranges are zoomable with the mouse wheel (see \ref QCPAxisRect::setRangeZoom, \ref QCPAxisRect::setRangeZoomAxes)
+                   ,iMultiSelect      = 0x004 ///< <tt>0x004</
