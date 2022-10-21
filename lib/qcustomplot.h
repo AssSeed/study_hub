af1d4c1f57b4224d555be711c50644e0e197370f
AssSeed <108232415+AssSeed@users.noreply.github.com>
2022-10-21 07:55:28 +0000

    moving code around

--- lib/qcustomplot.h
+++ lib/qcustomplot.h
@@ -1660,4 +1660,10 @@ protected:
   virtual void paintEvent(QPaintEvent *event);
   virtual void resizeEvent(QResizeEvent *event);
   virtual void mouseDoubleClickEvent(QMouseEvent *event);
-  virtual void mousePressEvent(QMouseEvent
+  virtual void mousePressEvent(QMouseEvent *event);
+  virtual void mouseMoveEvent(QMouseEvent *event);
+  virtual void mouseReleaseEvent(QMouseEvent *event);
+  virtual void wheelEvent(QWheelEvent *event);
+  
+  // introduced virtual methods:
+  virtual void draw(QCPPainter *painter
