b589fd70a17092685256d51bcb10addc139a4d1a
AssSeed <108232415+AssSeed@users.noreply.github.com>
2022-12-02 10:59:32 +0000

    Update PackagesAndModules.md

--- lib/qcustomplot.h
+++ lib/qcustomplot.h
@@ -1969,4 +1969,10 @@ typedef QMapIterator<double, QCPBarData> QCPBarDataMapIterator;
 typedef QMutableMapIterator<double, QCPBarData> QCPBarDataMutableMapIterator;
 
 
-class
+class QCP_LIB_DECL QCPBars : public QCPAbstractPlottable
+{
+  Q_OBJECT
+  /// \cond INCLUDE_QPROPERTIES
+  Q_PROPERTY(double width READ width WRITE setWidth)
+  Q_PROPERTY(QCPBars* barBelow READ barBelow)
+  Q_PROPERTY(
