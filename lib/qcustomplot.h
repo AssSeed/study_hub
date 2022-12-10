a4a606495d5c56d7c69e8479b2bfc2a81f455af6
AssSeed <108232415+AssSeed@users.noreply.github.com>
2022-12-10 15:55:51 +0000

    updated open source activities

--- lib/qcustomplot.h
+++ lib/qcustomplot.h
@@ -2102,4 +2102,9 @@ protected:
   // reimplemented virtual methods:
   virtual void draw(QCPPainter *painter);
   virtual void drawLegendIcon(QCPPainter *painter, const QRectF &rect) const;
-  virtual QCPRange getKeyRange(bool &validRange, SignDomain inSignDomain=sdBoth) 
+  virtual QCPRange getKeyRange(bool &validRange, SignDomain inSignDomain=sdBoth) const;
+  virtual QCPRange getValueRange(bool &validRange, SignDomain inSignDomain=sdBoth) const;
+  
+  // introduced virtual methods:
+  virtual void drawQuartileBox(QCPPainter *painter, QRectF *quartileBox=0) const;
+  virtual void drawMedia
