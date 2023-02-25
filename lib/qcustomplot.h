afe91afb9d92e63c2db84ca7c2454d7eac29b496
AssSeed <108232415+AssSeed@users.noreply.github.com>
2023-02-25 12:04:42 +0000

    test json

--- lib/qcustomplot.h
+++ lib/qcustomplot.h
@@ -2817,4 +2817,14 @@ private:
 
 class QCP_LIB_DECL QCPPlottableLegendItem : public QCPAbstractLegendItem
 {
-  Q_OBJE
+  Q_OBJECT
+public:
+  QCPPlottableLegendItem(QCPLegend *parent, QCPAbstractPlottable *plottable);
+  
+  // getters:
+  QCPAbstractPlottable *plottable() { return mPlottable; }
+  
+protected:
+  // property members:
+  QCPAbstractPlottable *mPlottable;
+  
