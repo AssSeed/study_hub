f95eaa74cdbd9600bca33fa0d9702b5e84ceb5a0
AssSeed <108232415+AssSeed@users.noreply.github.com>
2023-02-27 17:32:46 +0000

    simplification

--- lib/qcustomplot.h
+++ lib/qcustomplot.h
@@ -2866,4 +2866,10 @@ public:
   */
   enum SelectablePart { spNone       = 0x000  ///< <tt>0x000</tt> None
                        ,spLegendBox  = 0x001  ///< <tt>0x001</tt> The legend box (frame)
-                       ,spItems      = 0x002 
+                       ,spItems      = 0x002  ///< <tt>0x002</tt> Legend items individually (see \ref selectedItems)
+                      };
+  Q_FLAGS(SelectablePart SelectableParts)
+  Q_DECLARE_FLAGS(SelectableParts, SelectablePart)
+  
+  explicit QCPLegend();
+  virtual ~QCPLegend(
