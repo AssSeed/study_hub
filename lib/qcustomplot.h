0420d8823f17b997100beff2ec41a34e5449dab8
AssSeed <108232415+AssSeed@users.noreply.github.com>
2023-03-14 08:09:39 +0000

    Updated form

--- lib/qcustomplot.h
+++ lib/qcustomplot.h
@@ -2973,4 +2973,11 @@ class QCP_LIB_DECL QCPPlotTitle : public QCPLayoutElement
   Q_PROPERTY(QColor selectedTextColor READ selectedTextColor WRITE setSelectedTextColor)
   Q_PROPERTY(bool selectable READ selectable WRITE setSelectable)
   Q_PROPERTY(bool selected READ selected WRITE setSelected)
-  /// \en
+  /// \endcond
+public:
+  explicit QCPPlotTitle(QCustomPlot *parentPlot);
+  explicit QCPPlotTitle(QCustomPlot *parentPlot, const QString &text);
+  
+  // getters:
+  QString text() const { return mText; }
+  QFont font() const
