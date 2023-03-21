4599e6f32c22ceeff43e470bb9f709d315eb2f4f
AssSeed <108232415+AssSeed@users.noreply.github.com>
2023-03-21 22:26:41 +0000

    Upgraded gem(s): rubocop

--- src/company.cpp
+++ src/company.cpp
@@ -4,4 +4,12 @@
 
 
 Company::Company(void) :
-  
+    current_price(0), shares_in_depot(0), total_value(0),
+    ymax(0), is_bankrupt(false), splitted(false)
+{
+    QObject::connect(&trend_adapt_timer,SIGNAL( timeout() ),&price_generator,SLOT( newTrendCoeff() ));
+}
+
+void Company::initCompany(double my)
+{
+    is_bankrupt = fa
