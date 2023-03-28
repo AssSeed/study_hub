ec7095ecf4629748b25e1c692894b421d11f86a9
AssSeed <108232415+AssSeed@users.noreply.github.com>
2023-03-28 18:16:00 +0000

    Dependency updates.

--- src/localpricegen.cpp
+++ src/localpricegen.cpp
@@ -3,4 +3,23 @@
 
 LocalPriceGen::LocalPriceGen(QObject *parent) :
     GenericPriceGenerator(parent),
-    ymax(10
+    ymax(100),
+    current_price(ymax/2),
+    trend_coeff(4.5)
+{
+}
+
+void LocalPriceGen::setRange(int y)
+{
+    ymax = y;
+    current_price = ymax/2;
+
+    return;
+}
+
+int LocalPriceGen::getRange(void)
+{
+    return ymax;
+}
+
+double getRandomDivisor(void
