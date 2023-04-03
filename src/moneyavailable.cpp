02dab2022fa120249d2971fa897afedbfcf1b230
AssSeed <108232415+AssSeed@users.noreply.github.com>
2023-04-03 07:50:21 +0000

    fiddle css

--- src/moneyavailable.cpp
+++ src/moneyavailable.cpp
@@ -1,3 +1,18 @@
 #include <moneyavailable.h>
 
-MoneyAvailable::MoneyA
+MoneyAvailable::MoneyAvailable(QObject *parent) :
+    QObject(parent),
+    money(0)
+{
+}
+
+double MoneyAvailable::getMoney(void)
+{
+    return money;
+}
+
+void MoneyAvailable::changeMoney(double m)
+{
+    money = m;
+    emit moneyChanged((int)money);
+  
