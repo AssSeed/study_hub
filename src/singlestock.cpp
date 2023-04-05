9d08ce17bbd17029172e54233eb66599064c6abe
AssSeed <108232415+AssSeed@users.noreply.github.com>
2023-04-05 07:52:32 +0000

    Changed API

--- src/singlestock.cpp
+++ src/singlestock.cpp
@@ -16,4 +16,7 @@ SingleStock::SingleStock(QWidget *parent) :
     ui->plot->initCompanyPlot(xmax,100);
 
     QObject::connect(ui->plot,SIGNAL( priceChanged(int) ),ui->lcdPrice,SLOT( display(int) ));
-    QObject::connect(&main_timer,SIGNAL( timeout() ),ui->plot,S
+    QObject::connect(&main_timer,SIGNAL( timeout() ),ui->plot,SLOT( setData()));
+    QObject::connect(ui->buyButton,SIGNAL( clicked() ),this,SLOT( buyStock() ));
+    QObject::connect(ui->sellButton,SIGNAL( clicked() ),this,SLOT( sellStock() ));
+    QObject::connect(ui->orderStep,SIGNAL( valueChanged(int) ),thi
